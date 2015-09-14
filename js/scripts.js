e093a521cd52e9bc3a499e1760d53b307b2fcd53
Alex Costa & Ashlin Aronin <student@epicodus.com>
2015-09-14 10:03:13 -0700

    Fix script links and jQuery typos

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ $ (document).ready(function() {
         event.preventDefault();
 
         var inputtedFirstName = $("input#new-first-name").val();
-        var inputtedLastName = $("inputted#new-last-name").val();
-        var inputtedaddress = $("inputted#new-address").val();
+        var inputtedLastName = $("input#new-last-name").val();
+        var inputtedAddress = $("input#new-address").val();
         var newContact = {
                 firstName: inputtedFirstName,
                 lastName: inputtedLastName,
@@ -16,5 +16,13 @@ $ (document).ready(function() {
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
         $("input#new-address").val("");
+
+        $(".contact").last().click(function() {
+            $("#show-contact").show();
+            $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+            $(".first-name").text(newContact.firstName);
+            $(".last-name").text(newContact.lastName);
+            $(".address").text(newContact.address);
+        });
     });
 });
